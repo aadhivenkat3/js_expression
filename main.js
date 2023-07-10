66c9aae8252abe63273dfd68704e9a707d191742
aadhivenkat3 <sharmavenkat1@gmail.com>
2023-07-10 22:36:19 +0530

    add

---
+++ main.js
@@ -0,0 +1,70 @@
+// js expression 1
+
+var a=20, b=4, c=3;
+
+console.log(a==b)&&(b==c);
+
+// js expression 2
+
+var a=20, b=2;
+
+console.log((a++)+(++a)-(a+b));
+
+//js expression 3
+
+var a=5, b=5, c=10;
+
+console.log(a==b)||(c<b);
+
+//js expression 4
+
+var a=1, b=2, c=3;
+
+console.log((a*b)&&(b*c)&&(c*a));
+
+
+//js expression 5
+
+var a=2, b=4, c=3;
+
+console.log(a<=b)&&(a%b<c);
+
+// js expression 6
+
+var a=7, b=5;
+
+console.log((a+b)&&(a-b)&&(a%b));
+
+// js expression 7
+
+var a=2, b=3;
+
+console.log(a<b)||(b<c)||(a<c);
+
+
+// js expression 8
+
+var a=1, b=2, c=3;
+
+console.log(!((a=b)&&(b===c))); 
+
+// js expression 9
+
+var a=4, b=5;
+
+console.log(a!=b)&&(a<b);
+
+// js expression 10
+
+var a=10, b=5, c=20;
+
+console.log(a<b)&&(a++)<=c;
+
+// js expression 11
+
+var a=78;
+    c=--a;
+	
+	console.log(--a);
+	console.log(a++);
+	console.log(++a);
